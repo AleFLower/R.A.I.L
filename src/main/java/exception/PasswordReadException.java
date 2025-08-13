31bcaccd3c1ec6ca88643ef5bb67714e74665a6d
AleFlower <alessiofior3@gmail.com>
2025-08-13 11:39:08 +0200

    fixed smell

--- src/main/java/exception/PasswordReadException.java
+++ src/main/java/exception/PasswordReadException.java
@@ -1,7 +1,5 @@
 package exception;
 
-import java.io.IOException;
-
 public class PasswordReadException extends Exception{
     public PasswordReadException(String message){
         super(message);
