db2ddacd301245297e137ec5137bb3585c1e24c1
AleFlower <alessiofior3@gmail.com>
2025-08-22 14:32:15 +0200

    fixed smells

--- src/main/java/dao/ReportRepository.java
+++ src/main/java/dao/ReportRepository.java
@@ -16,15 +16,14 @@ public class ReportRepository {
 
     public void sendReport(RailwayAsset asset, String usercode) throws ReportAlreadyExistsException {
         for (RailwayAsset reportAsset : reports) {
-            if (asset.getAssetType().equals(AssetType.TRACK)) {
-                if (reportAsset.getAssetInfo().equals(asset.getAssetInfo())
-                        && reportAsset.getLocation().equals(asset.getLocation())) {
-                    throw new ReportAlreadyExistsException("Track with same number and location already reported.");
-                }
-            } else if (asset.getAssetType().equals(AssetType.LEVELCROSSING)) {
-                if (reportAsset.getAssetInfo().equals(asset.getAssetInfo())) {
-                    throw new ReportAlreadyExistsException("Level crossing with same code already reported.");
-                }
+            if (asset.getAssetType().equals(AssetType.TRACK)
+                    && reportAsset.getAssetInfo().equals(asset.getAssetInfo())
+                    && reportAsset.getLocation().equals(asset.getLocation())) {
+                throw new ReportAlreadyExistsException("Track with same number and location already reported.");
+            }
+            else if (asset.getAssetType().equals(AssetType.LEVELCROSSING)
+                    && reportAsset.getAssetInfo().equals(asset.getAssetInfo())) {
+                throw new ReportAlreadyExistsException("Level crossing with same code already reported.");
             }
         }
         registerReport(usercode, asset);
