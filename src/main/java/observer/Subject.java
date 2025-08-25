1148d59e6d5e5ac409141c539d0833b22c8a3616
AleFlower <alessiofior3@gmail.com>
2025-08-25 16:27:20 +0200

    update

--- src/main/java/observer/Subject.java
+++ src/main/java/observer/Subject.java
@@ -3,12 +3,13 @@ package observer;
 import java.util.List;
 
 public abstract class Subject {
-    public void attach(Observer o){this.observerList.add(o);};
+    public void attach(Observer o){this.observerList.add(o);}
     public void detach(Observer o){
         int i=this.observerList.indexOf(o);
         if(i>=0) {
             this.observerList.remove(i);
-        }};
+        }
+    }
     protected abstract void notifyObservers();
     protected List<Observer> observerList;
 }
